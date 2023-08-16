4d8e3099f6afaa0a617e48032ed4bddf909a903f
Allen Wang <allenw@mit.edu>
2023-08-16 12:58:11 -0400

    WIP

--- rag/embed_cmod.py
+++ rag/embed_cmod.py
@@ -1,32 +1,23 @@
 from dotenv import load_dotenv
 load_dotenv()
+import sys
+# from embedding.llm_utils import embed_sentences
 import chromadb
-from chromadb.utils import embedding_functions
 from tqdm import tqdm
 import pickle
-import openai
-import os
-import time
-openai.api_key = os.getenv("OPENAI_API_KEY")
-# openai.api_type = "azure"
-# openai.api_base = "https://test-oai69420.openai.azure.com/"
-# openai.api_version = "2023-05-15"
+import itertools
 
-def process_text_data(data, topics_exclude=['rf_monitor'], max_string_length=4*2048):
-    # Exclude rf_monitor as it tends to have a lot of raw data for whatever reason.
+def process_text_data(data):
     new_data = dict()
     shots = list(data.keys())
-    subset_data = {k: data[k] for k in shots}
-    for shot, shot_data in tqdm(subset_data.items()):
-        shot_text = f"CMOD Shot Number: {shot}\n"
+    subset_data = {k: data[k] for k in shots[:5]}
+    for shot, shot_data in subset_data.items():
+        print(f"shot: {shot}")
+        shot_text = "Shot: {shot}\n"
         for entry in shot_data:
-            if entry['topic'] in topics_exclude:
-                continue
-            entry_string = f"ENTRY TOPIC: {entry['topic']}\nENTRY USER: {entry['user']}\nENTRY TEXT:\n{entry['text']}\n"
+            entry_string = f"entry topic: {entry['topic']}\nentry user: {entry['user']}\nentry text:"
             shot_text += "\n" + entry_string
-        
-        if len(shot_text) <= max_string_length:
-            new_data[shot] = shot_text
+        new_data[shot] = shot_text
     return new_data
 
 def read_file(pickle_file):
@@ -35,48 +26,17 @@ def read_file(pickle_file):
         data = pickle.load(f)
     return data
 
-def batch_dictionary(dictionary, batch_size):
-    keys = list(dictionary.keys())
-    batches = []
-
-    for i in range(0, len(keys), batch_size):
-        batch = {key: dictionary[key] for key in keys[i:i+batch_size]}
-        batches.append(batch)
-
-    return batches
-
-def get_chat_completion(system_message, user_message, model="gpt-3.5-turbo"):
-    completion = openai.ChatCompletion.create(
-      model=model,
-      messages=[
-        {"role": "system", "content": system_message},
-        {"role": "user", "content": user_message}
-      ]
-    )
-
-    return completion.choices[0].message.content
-
-def main(targt):            
+def main(targt):
     data = read_file(targt)
     processed_text_data = process_text_data(data)
-    openai_ef = embedding_functions.OpenAIEmbeddingFunction(
-                api_key=openai.api_key,
-                model_name="text-embedding-ada-002"
-            )
-
-    client = chromadb.PersistentClient(path="/home/awang/chatcmod_db")
-    collection = client.get_or_create_collection("cmod_text-embedding-ada-002", embedding_function=openai_ef)
-
-    batches = batch_dictionary(processed_text_data, 500)
-    for batch in tqdm(batches):
-        keys = list(batch.keys())
-        values = list(batch.values())
-        import pdb; pdb.set_trace()
-        collection.add(ids = keys, documents = values)
-        print(f'added batch of {len(keys)} documents to collection')
-        time.sleep(60) # Wait a minute to not exceed rate limits.
+    client = chromadb.PersistentClient(path="db")
+    collection = client.get_or_create_collection("cmod_embeddings")
+    keys = list(processed_text_data.keys())
+    values = list(processed_text_data.values())
+    collection.add(ids = keys,
+                   documents = values)
     # print(f'embeddings: {embeddings}')
 
 if __name__ == '__main__':
-    pickle_file = "/home/awang/logbook.pkl"
+    pickle_file = "/nobackup1/allenw/Scratch/logbook.pkl"
     main(pickle_file)
