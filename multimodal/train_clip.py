bb31f671461e065f17a275026597e8a18e9414e4
Viraj Mehta <virajm@cs.cmu.edu>
2023-08-23 09:45:48 -0500

    updated code for multimodal stuff

--- multimodal/train_clip.py
+++ multimodal/train_clip.py
@@ -1,15 +1,16 @@
 import torch
 import pickle
 from torch.utils.data import Dataset, DataLoader
+import numpy as np
 from torch import nn
 
 class PairedDataset(Dataset):
     def __init__(self, fn1, fn2):
-        self.keys = list(dict1.keys())
         with open(fn1, 'rb') as f:
             self.dict1 = pickle.load(f)
         with open(fn2, 'rb') as f:
             self.dict2 = pickle.load(f)
+        self.keys = list(self.dict1.keys())
 
     def __len__(self):
         return len(self.keys)
@@ -18,7 +19,7 @@ class PairedDataset(Dataset):
         key = self.keys[idx]
         return self.dict1[key], self.dict2[key]
 
-     def get_embedding_dim1(self):
+    def get_embedding_dim1(self):
         return len(next(iter(self.dict1.values())))
 
     def get_embedding_dim2(self):
@@ -26,7 +27,7 @@ class PairedDataset(Dataset):
 
 
 
-def main():
+def main(dict1, dict2):
     dataset = PairedDataset(dict1, dict2)
     embedding_dim1 = dataset.get_embedding_dim1()
     embedding_dim2 = dataset.get_embedding_dim2()
@@ -47,12 +48,15 @@ def main():
             z1 = encoder1(batch1)
             z2 = encoder2(batch2)
 
-            e1 = torch.linalg.vector_norm(z1, dim=1)
-            e2 = torch.linalg.vector_norm(z2, dim=1)
-            logits = e1 @ e2 * torch.exp(t)
+            e1 = torch.linalg.vector_norm(z1, dim=1)[:, None]
+            e2 = torch.linalg.vector_norm(z2, dim=1)[:, None]
+            breakpoint()
+            norm_z1  = z1 / e1
+            norm_z2 = z2 / e2
+            logits = norm_z1 @ norm_z2.T * np.exp(temp)
             labels = torch.arange(len(batch1))
-            loss1 = cross_entropy(logits, labels, axis=0)
-            loss2 = cross_entropy(logits, labels, axis=1)
+            loss1 = cross_entropy(logits, labels)
+            loss2 = cross_entropy(logits.T, labels)
             loss = (loss1 + loss2) / 2
             loss.backward()
             optimizer.step()
@@ -60,3 +64,8 @@ def main():
         print(f'Epoch {epoch}, Loss: {loss.item()}')
     torch.save(encoder1, 'encoder1_model.pth')
     torch.save(encoder2, 'encoder2_model.pth')
+
+if __name__ == '__main__':
+    vae_embeddings = 'vae_encodings.pkl'
+    text_embeddings = 'text_embeddings.pkl'
+    main(vae_embeddings, text_embeddings)
