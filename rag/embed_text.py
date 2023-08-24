08155d0b72895be555ce6080a727dd552255c353
Joseph Abbate <joseph.abbate7@gmail.com>
2023-08-24 12:26:38 -0400

    add miniproposals

--- rag/embed_text.py
+++ rag/embed_text.py
@@ -1,41 +1,26 @@
 import chromadb
 import sys
 import os
-
+from text_helpers import make_document_dic_from_string
 
 def read_file(file_name):
     with open(file_name) as f:
         text = f.read()
     return text
 
-def chunk_text(text, chunking=1700):
-    # Average shotlog entry is around 1700 characters
-    total_len = len(text)
-    ind = 0
-    chunks = []
-
-    while ind < total_len:
-        chunks.append(text[ind:ind+chunking])
-        ind += chunking
-        
-    return chunks
-
-def main(text_dir, embedding_title='text'):
+# document_type "miniproposal"
+def main(text_dir, document_type):
     files = os.listdir(text_dir)
     text_data = {}
     for fn in files:
-        text = read_file(text_dir+fn)
-        chunks = chunk_text(text)
-        for i, chunk in enumerate(chunks):
-            text_data[fn+f'_{i}'] = chunk
+        text = read_file(os.path.join(text_dir,fn))
+        text_data.update(make_document_dic_from_string(text, fn, document_type))
 
     client = chromadb.PersistentClient(path='db')
-    collection = client.get_or_create_collection(f'{embedding_title}_embeddings')
+    collection = client.get_or_create_collection(f'{document_type}_embeddings')
     keys = list(text_data.keys())
     values = list(text_data.values())
     collection.add(ids = keys, documents = values)
-
-    
 
 if __name__ == '__main__':
     main(sys.argv[1], sys.argv[2])
